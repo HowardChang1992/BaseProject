82d04cc514a092b7ce8773a1a8bc9f50061f83f1
Howard Chang <howard.chang.developer@gmail.com>
2017-09-13 11:53:20 +0800

    create base implement model class

--- baseproject-android/app/src/main/java/com/chang/template/activity/home/MainModel.java
+++ baseproject-android/app/src/main/java/com/chang/template/activity/home/MainModel.java
@@ -2,6 +2,7 @@ package com.chang.template.activity.home;
 
 import android.content.Context;
 
+import com.chang.template.activity.BaseImplementModel;
 import com.chang.template.net.Networks;
 import com.chang.template.net.model.DemoResponse;
 
@@ -10,7 +11,7 @@ import io.reactivex.Observable;
 /**
  * Created by Howard Chang on 2017/4/18
  */
-public class MainModel implements MainContract.Model {
+public class MainModel extends BaseImplementModel implements MainContract.Model {
 
     @Override
     public Observable<DemoResponse> performRequest(Context context) {
