a7c9ea1a9e700ddc0b54c1e5c161125ade270520
Howard Chang <howard.chang.developer@gmail.com>
2018-01-31 13:06:09 +0800

    upgrade gradle version to 3.0.1

--- baseproject-android/app/src/main/java/com/chang/template/MyApplication.java
+++ baseproject-android/app/src/main/java/com/chang/template/MyApplication.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.net.Uri;
 import android.support.multidex.MultiDexApplication;
 
-import com.squareup.picasso.LruCache;
-import com.squareup.picasso.Picasso;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -24,23 +22,7 @@ public class MyApplication extends MultiDexApplication {
     }
 
     public static void buildPicasso(Context context) {
-        Picasso.Builder builder = new Picasso.Builder(context);
 
-        LruCache cache = new LruCache(5 * 1024 * 1024);
-        builder.memoryCache(cache);
-
-        ExecutorService executorService = Executors.newFixedThreadPool(8);
-        builder.executor(executorService);
-
-        builder.listener(new Picasso.Listener() {
-            @Override
-            public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
-                exception.printStackTrace();
-            }
-        });
-
-        Picasso picasso = builder.build();
-        Picasso.setSingletonInstance(picasso);
     }
 
 }
